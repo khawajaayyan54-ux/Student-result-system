a6c33e5e48c1cbb168af895f1349f3a318191bf8
khawajaayyan54-ux <khawajaayyan54@gmail.com>
2025-12-28 18:45:38 +0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -4,11 +4,29 @@ student = {"ayyan":
             "urdu" : 35,
             "physics" : 45,},
             
-            "Yousuf":
+            "yousuf":
            {"maths": 56,
             "english" : 78,
             "urdu" : 35,
             "physics" : 45,},
+            
+            "sam":
+           {"maths": 80,
+            "english" : 78,
+            "urdu" : 54,
+            "physics" : 65,},
+            
+            "mick":
+           {"maths": 35,
+            "english" : 79,
+            "urdu" : 97,
+            "physics" : 34,},
+            
+            "jordan":
+           {"maths": 28,
+            "english" : 98,
+            "urdu" : 38,
+            "physics" : 84,},
 
             "liya":
            {"maths": 56,
@@ -19,33 +37,46 @@ student = {"ayyan":
 
 def code():
     user = input("Enter the student name: ").lower()
-    if user in student:
-        total = 0
-        for marks in student[user].values():
-            total += marks
-        percentage = total/400*100
-        print("SUBJECT AND MARKS: ")
-        for subject, mark in student[user].items():
-            print(f"{subject.capitalize()} : {mark}")
-        
-        if percentage >= 80:
-            grade = "A+"
-        elif percentage >= 70:
-            grade = "A"
-        elif percentage >= 60:
-            grade = "B"
-        elif percentage >= 40:
-            grade = "C"
-        else:
-            grade = "F"
-        print("STUDENT DETAILS: ")    
-        if percentage >= 40:
-            print(f"Name : {user}\nTotal Marks : {total}\nStatus : Passed\nPercentage: {percentage}%\nGrade : {grade}")
-        else:
-            print(f"Name : {user}\nTotal Marks : {total}\nStatus : Failed\nPercentage: {percentage}%\nGrade : {grade}")
+    if user == "all name":
+        print("STUDENT NAME: ")
+        for st_name, st_data in student.items():
+            print(st_name.capitalize())
+    
+    elif user == "all details":
+        for st_name, st_data in student.items():
+            print(f"{st_name.capitalize()}: ")
+            for subject, mark in st_data.items():
+                print(f"{subject.capitalize()} : {mark}")
+            print()
 
-    else:
-        print("user not found")
+    else:        
+        if user in student:
+            total = 0
+            for marks in student[user].values():
+                total += marks
+            percentage = total/400*100
+            print("SUBJECT AND MARKS: ")
+            for subject, mark in student[user].items():
+                print(f"{subject.capitalize()} : {mark}")
+            
+            if percentage >= 80:
+                grade = "A+"
+            elif percentage >= 70:
+                grade = "A"
+            elif percentage >= 60:
+                grade = "B"
+            elif percentage >= 40:
+                grade = "C"
+            else:
+                grade = "F"
+            print("STUDENT DETAILS: ")    
+            if percentage >= 40:
+                print(f"Name : {user.capitalize()}\nTotal Marks : {total}\nStatus : Passed\nPercentage: {percentage}%\nGrade : {grade}")
+            else:
+                print(f"Name : {user.capitalize()}\nTotal Marks : {total}\nStatus : Failed\nPercentage: {percentage}%\nGrade : {grade}")
+
+        else:
+            print("user not found")
 
 code()
 
