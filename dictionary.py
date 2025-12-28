b7d29694135599e1edfeccddbd4bf6563e37c929
Ace <khawajaayyan54@gmail.com>
2025-12-29 01:05:22 +0500

    Add files via upload
    thats  just an prototype to give idea abt dictionary of this code

---
+++ dictionary.py
@@ -0,0 +1,39 @@
+# code's prototype:
+
+# not in use currently: 
+
+student = {"ayyan":
+           {"maths": 56,
+            "english" : 78,
+            "urdu" : 35,
+            "physics" : 45,},
+            
+            "yousuf":
+           {"maths": 56,
+            "english" : 78,
+            "urdu" : 35,
+            "physics" : 45,},
+            
+            "sam":
+           {"maths": 80,
+            "english" : 78,
+            "urdu" : 54,
+            "physics" : 65,},
+            
+            "mirha":
+           {"maths": 35,
+            "english" : 79,
+            "urdu" : 97,
+            "physics" : 34,},
+            
+            "jordan":
+           {"maths": 28,
+            "english" : 98,
+            "urdu" : 38,
+            "physics" : 84,},
+
+            "liya":
+           {"maths": 56,
+            "english" : 78,
+            "urdu" : 35,
+            "physics" : 45,}}
