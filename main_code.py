a764a49c412947747b83b0da3891976969f9bccf
Ace <khawajaayyan54@gmail.com>
2025-12-29 18:40:42 +0500

    Refactor student management using Json file

--- main_code.py
+++ main_code.py
@@ -1,67 +1,76 @@
-student = {}
-print("MENU: \n1. All Name\n2. All details\n3. Student name:")
+import json
+import os
+filename = "students.json"
 
-try:
-    with open("student.txt","r") as file:
-        for line in file:
-            parts = line.strip().split(",")
-            name = parts[0]
-            student[name] = {}
-            for item in parts[1:]:
-                subject, mark = item.split("=")
-                student[name][subject] = int(mark)
-except FileNotFoundError:
-    print("Error, file doesn't found, please create file first ")
-except Exception as e:
-    print("Error reading file: ", e)
+def load_students(filename):
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+        
+    except FileNotFoundError:
+        print("Error: file not found.")
+        return {}
+    except json.JSONDecodeError:
+        print("json file is corrputed. ")
+        return{}
 
-def code():
-    user = input("Enter the command or student name: ").lower()
+def show_student_name(students):
+    print("STUDENT NAME: ")
+    for name in students:
+        print("-",name.capitalize())
+
+def show_student_details(students):
+     for name, subjects in students.items():
+        print(f"{name.capitalize()}:")
+        for subject, mark in subjects.items():
+            print(f"{subject.capitalize()} : {mark}")
+        print()
+
+def show_student_result(students, user):
+        total = sum(students[user].values())
+        percentage = total/(len(students[user])*100)*100
+
+        print("SUBJECT AND MARKS: ")
+        for subject, mark in students[user].items():
+            print(f"{subject.capitalize()} : {mark}")
+        print()
+                       
+        if percentage >= 80:
+            grade = "A+"
+        elif percentage >= 70:
+            grade = "A"
+        elif percentage >= 60:
+            grade = "B"
+        elif percentage >= 40:
+            grade = "C"
+        status = "passed" if percentage >= 40 else "failed"
+        print(f"Name : {user.capitalize()}\nTotal Marks : {total}\nStatus : {status}\nPercentage: {percentage}%\nGrade : {grade}")
+def main():
+    students = load_students(filename)
+    if not students:
+        return
+    print("MENU: \n1. All Name\n2. All details\n3. Student name:")
+
+    user = input("enter ur command or student name: ").lower()
+
     if user == "all name":
-        print("STUDENT NAME: ")
-        for st_name, st_data in student.items():
-            print(st_name.capitalize())
-    
+        show_student_name(students)
     elif user == "all details":
-        for st_name, st_data in student.items():
-            print(f"{st_name.capitalize()}: ")
-            for subject, mark in st_data.items():
-                print(f"{subject.capitalize()} : {mark}")
-            print()
+        show_student_details(students)
+    elif user in students:
+        show_student_result(students, user)
+    else:
+        print("user not found")
 
-    else:        
-        if user in student:
-            total = sum(student[user].values())
-            percentage = total/(len(student[user])*100)*100
-            print("SUBJECT AND MARKS: ")
-            for subject, mark in student[user].items():
-                print(f"{subject.capitalize()} : {mark}")
-            print()
-            
-            if percentage >= 80:
-                grade = "A+"
-            elif percentage >= 70:
-                grade = "A"
-            elif percentage >= 60:
-                grade = "B"
-            elif percentage >= 40:
-                grade = "C"
-            else:
-                grade = "F"
-            status = "passed" if percentage >= 40 else "failed"
-            print("STUDENT DETAILS: ")    
-            print(f"Name : {user.capitalize()}\nTotal Marks : {total}\nStatus : {status}\nPercentage: {percentage}%\nGrade : {grade}")
 
-        else:
-            print("user not found")
-
-code()
-
+main()
 while True:
     startcode = input("Type y to continue(y/n): ")
-    if startcode == "y":
-        code()
-    elif startcode == "n":
+    if startcode == "n":
+        print("exiting program")
         break
+    
+    elif startcode == "y":
+        main()
     else:
-        print("error smth went wrong")    
+        print("wrong input")
